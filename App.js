2ee6f229a940442fb118e8a9e03a2a8e5919066e
EdikEd <edik.ergart2017@gmail.com>
2023-09-05 13:02:45 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, ImageBackground } from 'react-native';
+const logoImg = require("./assets/icon.png");
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return <View style={{flex: 1,backgroundColor:'plum', padding: 16}}>
+    <Image source={logoImg} style={{width: '100%', height: 200}}></Image>
+    <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quam modi temporibus reprehenderit aut id pariatur rerum tenetur laboriosam ab quos aliquam labore cumque! Labore, nesciunt. Facere beatae dolor magnam!
+    </Text>
+    <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quam modi temporibus reprehenderit aut id pariatur rerum tenetur laboriosam ab quos aliquam labore cumque! Labore, nesciunt. Facere beatae dolor magnam!
+    </Text>
+    <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quam modi temporibus reprehenderit aut id pariatur rerum tenetur laboriosam ab quos aliquam labore cumque! Labore, nesciunt. Facere beatae dolor magnam!
+    </Text>
+    <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quam modi temporibus reprehenderit aut id pariatur rerum tenetur laboriosam ab quos aliquam labore cumque! Labore, nesciunt. Facere beatae dolor magnam!
+    </Text>
+    <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quam modi temporibus reprehenderit aut id pariatur rerum tenetur laboriosam ab quos aliquam labore cumque! Labore, nesciunt. Facere beatae dolor magnam!
+    </Text>
+    <Text>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quam modi temporibus reprehenderit aut id pariatur rerum tenetur laboriosam ab quos aliquam labore cumque! Labore, nesciunt. Facere beatae dolor magnam!
+    </Text>
+    <Image source={logoImg} style={{width: '100%', height: 200}}></Image>
+  </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
